5275160ebf89e21f42783b9b29cea5f7dd1eadcb
sshekhawat <sshekhawat@L-NL-20568>
2019-11-20 16:06:41 +0100

    added entire project and simple space test

--- testframework/CDAAS_TestFramework/UnitTests.cs
+++ testframework/CDAAS_TestFramework/UnitTests.cs
@@ -8,7 +8,6 @@ namespace UnitTestProjectCoreMesTest
         [TestMethod]
         public void TestMethod1()
         {
-
             //kept on purpose
             Assert.IsFalse(true);
         }
